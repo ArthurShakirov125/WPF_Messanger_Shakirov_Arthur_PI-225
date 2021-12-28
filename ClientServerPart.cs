f347a3e82f95f4caef5a2193b5bb69917000fdea
Shakirov_Arthur <Wolverane32@gmail.com>
2021-12-28 16:45:56 +0500

    Проект мессенджер с сервером

---
+++ ClientServerPart.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+
+namespace MessangerProject
+{
+    public class MyMessage
+    {
+        public string UserName { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string MessageText { get; set; }
+    }
+        
+    public class ClientServerPart
+    {
+        private TcpClient _user;
+        private string _UserName;
+        private NetworkStream _stream;
+        private StreamWriter _sw;
+        private StreamReader _sr;
+
+
+        public ClientServerPart(string UserName)
+        {
+            _UserName = UserName;
+            _user = new TcpClient();
+            _user.Connect("localhost", 8000);
+
+            _stream = _user.GetStream();
+            _sw = new StreamWriter(_stream);
+            _sr = new StreamReader(_stream);
+        }
+
+
+        public void SendMessage(string Text)
+        {
+            _sw.WriteLine(MadeMessage(Text));
+            _sw.Flush();
+        }
+
+        public MyMessage RecieveMessage()
+        {
+            return JsonConvert.DeserializeObject<MyMessage>(_sr.ReadLine());
+        }
+
+        private string MadeMessage(string Text)
+        {
+            MyMessage message = new MyMessage()
+            {
+                Time = DateTime.Now,
+                UserName = _UserName,
+                MessageText = Text,
+            };
+
+            return JsonConvert.SerializeObject(message);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MessangerProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string UserName;
+        private ClientServerPart _client;
+        private MyMessage messageForChat;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UserName = UsernameUI.Text;
+            _client = new ClientServerPart(UserName);
+        }
+
+        private void Run()
+        {
+            messageForChat = _client.RecieveMessage();
+            TextBlock textBlock = new TextBlock()
+            {
+                Text = $"{messageForChat.Time.ToString("HH:mm")} {messageForChat.UserName}:{messageForChat.MessageText}",
+                TextWrapping = TextWrapping.Wrap,
+            };
+
+            MessagesField.Children.Add(textBlock);
+
+        }
+
+        private void Messadge_Click(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = new TextBlock()
+            {
+                Text = $"[{DateTime.Now.ToString("HH:mm")}] {UserName} : {MessageText.Text}",
+                TextWrapping = TextWrapping.Wrap,
+            };
+
+            MessagesField.Children.Add(textBlock);
+        }
+
+
+        private void EnterMessage(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                _client.SendMessage(MessageText.Text);
+            }
+
+        }
+
+        private void Edit_Username(object sender, MouseButtonEventArgs e)
+        {
+            EditUserNameField.Visibility = Visibility.Visible;
+            EditUserNameField.Focus();
+        }
+
+        private void Edit(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                UsernameUI.Text = EditUserNameField.Text;
+                UserName = UsernameUI.Text;
+                EditUserNameField.Text = "";
+                EditUserNameField.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
